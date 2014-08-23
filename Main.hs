dc940289154f2fe924b703b9c06cf4fd021296f5
Peter Harpending <pharpend2@gmail.com>
2014-08-23 14:48:47 -0600

    Add a main file.

---
+++ Main.hs
@@ -0,0 +1,15 @@
+{-|
+Module       : Main
+Description  : Runs a simulation
+Copyright    : None
+License      : PublicDomain
+Maintainer   : Peter Harpending <pharpend2@gmail.com>
+Stability    : experimental
+Portability  : Linux-3.16
+
+-}
+
+module Main where
+
+main :: IO ()
+main = return ()
