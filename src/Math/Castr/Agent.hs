fcf8eea7bcde3c8511d1e2a382d02902a76a23bb
Peter Harpending <pharpend2@gmail.com>
2014-08-23 14:59:38 -0600

    Separate library and executable components.

---
+++ src/Math/Castr/Agent.hs
@@ -0,0 +1,12 @@
+{-|
+Module       : Math.Castr.Agent
+Description  : Code for agents
+Copyright    : None
+License      : PublicDomain
+Maintainer   : Peter Harpending <pharpend2@gmail.com>
+Stability    : experimental
+Portability  : Linux-3.16
+
+-}
+
+module Math.Castr.Agent where
