654dbde1917cd585c1bdc40d020ac088629bb624
Peter Harpending <pharpend2@gmail.com>
2014-08-23 14:47:29 -0600

    Add skel file and .gitignore.

---
+++ skel/Skel.hs
@@ -0,0 +1,12 @@
+{-|
+Module       : Skel
+Description  : Skeleton file
+Copyright    : None
+License      : PublicDomain
+Maintainer   : Peter Harpending <pharpend2@gmail.com>
+Stability    : experimental
+Portability  : Linux-3.16
+
+-}
+
+module Skel where
