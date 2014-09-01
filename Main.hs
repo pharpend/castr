bfd9dba4d4fd40bb7f562c389e5df8b045093cfd
Peter Harpending <pharpend2@gmail.com>
2014-08-31 19:36:20 -0600

    Added a command line interface, as well as some examples.

--- Main.hs
+++ Main.hs
@@ -11,47 +11,42 @@ Portability  : Linux-3.16
 
 module Main where
 
-import           Control.Applicative
-import qualified Data.Aeson as Aeson
+import           Data.Aeson
 import           Data.Aeson.Encode.Pretty
-import qualified Data.ByteString as Bs
 import qualified Data.ByteString.Lazy as Bl
-import           Data.Yaml
+import           Data.Monoid
 import           Math.Castr.Agent
+import           System.Environment
 import           System.IO
 
-data MainInput = MainInput { number :: Int
-                           , heritability :: Double
-                           , printer :: Printer
-                           }
-
-data Printer = JsonPretty
-             | JsonUgly
-             | Yaml
-
-instance FromJSON MainInput where
-  parseJSON (Object v) = MainInput <$> v .:? "n" .!= 1000
-                                   <*> v .:? "h" .!= 0.5
-                                   <*> v .:? "printer" .!= JsonPretty
-  parseJSON _ = fail "Not an object"
-
-instance FromJSON Printer where
-  parseJSON (String s) = case s of
-    "json-pretty" -> return JsonPretty
-    "json-ugly" -> return JsonUgly
-    "yaml" -> return Yaml
-    _ -> fail "Invalid printer."
-  parseJSON _ = fail "Not a string"
-
-genPool :: Bs.ByteString -> IO ()
-genPool inputBytes = case decodeEither inputBytes of
-  Left err -> hPutStrLn stderr err
-  Right (MainInput n h p) -> do
-    pool <- mkPool n h
-    case p of
-      JsonPretty -> Bl.hPut stdout $ encodePretty pool
-      JsonUgly -> Bl.hPut stdout $ Aeson.encode pool
-      Yaml -> Bs.hPut stdout $ encode pool
-
 main :: IO ()
-main = hSetBinaryMode stdin True >> Bs.hGetContents stdin >>= genPool
+main = runArguments =<< getArgs
+
+-- |I can't get any command line parsers to work correctly, so I'm
+-- doing it by hand
+help :: IO ()
+help = Bl.hPut stdout . mappend
+ "Things you can do are listed. Unless otherwise noted, all output \n\
+ \is in compressed JSON.\n" .
+  mconcat . map (mappend "\n\t") $
+    [ "mkpool N H\tGenerate a pool with N agents, a heritability of H."
+    , "splpool F GS\tSplit the pool read from F into groups of size GS."
+    , "\n"
+    ]
+  
+runArguments :: [String] -> IO ()
+runArguments args = case args of
+    ["mkpool", n, h] -> printPool =<< mkPool (read n) (read h)
+    ["splpool", fpath, groupSize] -> splPool fpath (read groupSize)
+    _ -> help
+
+printPool :: Pool -> IO ()
+printPool = Bl.hPut stdout . encodePretty
+
+splPool :: FilePath -> Int -> IO ()
+splPool fpath subsize = do
+  poolBytes <- Bl.readFile fpath
+  case eitherDecode poolBytes of
+    Left errorMessage -> hPutStr stderr errorMessage
+    Right pl -> do
+      Bl.hPut stdout . encodePretty $ splitPool subsize pl
